1914ca4dd57901e53162d50275d043f6d0f0d77b
Aaron Foucheaux <afoucheaux@gmail.com>
2021-06-01 16:47:42 -0500

    Basic router setup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import FrontPage from '../FrontPage/FrontPage.js';
+import Article from '../Article/Article.js';
 
 
 function App() {
+
   return (
-    <p>test</p>
+    <Switch classname='app'>
+      <Route exact path='/article' component={ Article }/>
+      <Route path='/' component={ FrontPage }/>
+    </Switch>
   );
 }
 
---
+++ src/components/Article/Article.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Article.css';
+
+function Article() {
+
+  return (
+    <p>Test2</p>
+  )
+}
+
+export default Article;
